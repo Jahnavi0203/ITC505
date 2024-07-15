ea0ce283532f0d0f9dc84bcd0ce36635e69fc9a4
Jahnavi0203 <jj2433@nau.edu>
2024-07-15 03:00:26 -0700

    Update script.js

--- mid-term-project/script.js
+++ mid-term-project/script.js
@@ -5,7 +5,7 @@ const story = {
             { text: "Investigate the anomaly", consequence: "investigateAnomaly" },
             { text: "Land on the planet", consequence: "landOnPlanet" }
         ],
-        image: "https://static.wikia.nocookie.net/nomanssky_gamepedia/images/d/df/Ano_space.jpg/revision/latest?cb=20170818201220"
+        image: "https://i.ytimg.com/vi/LrrNu_m_9K4/maxresdefault.jpg"
     },
     investigateAnomaly: {
         text: "As you get closer, you realize it's a wormhole. Do you enter it or retreat?",
